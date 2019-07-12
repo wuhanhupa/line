9da2094e6f40f89780da97a25b90f8523675e2d5
wuhanhupa <wuhanhupa@protonmail.com>
2019-07-12 13:41:59 +0800

    ss

--- Runtime/Data/_fields/bnb.btchanges_bz.php
+++
@@ -1,1 +0,0 @@
-a:4:{i:0;s:2:"id";i:1;s:6:"market";i:2;s:6:"number";s:5:"_type";a:3:{s:2:"id";s:7:"int(11)";s:6:"market";s:12:"varchar(255)";s:6:"number";s:12:"varchar(255)";}}
--- Runtime/Data/_fields/bnb.btchanges_rate.php
+++
@@ -1,1 +0,0 @@
-a:5:{i:0;s:2:"id";i:1;s:4:"rate";i:2;s:7:"addtime";s:3:"_pk";s:2:"id";s:5:"_type";a:3:{s:2:"id";s:7:"int(11)";s:4:"rate";s:12:"varchar(255)";s:7:"addtime";s:12:"varchar(255)";}}
